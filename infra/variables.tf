65ce79bb4d04df76b4af6666e70a1200d92c2d8e
Kilian Roig <kilianroig@MacBook-Pro-de-Kilian.local>
2026-01-30 23:44:11 +0700

    chore: add initial terraform infrastructure

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "cloud-event-platform"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
