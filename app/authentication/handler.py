84afdec5c244836d14aabd164f7dc1ecc77dee8b
Kilian.roig <roigmartinezkilian.@gmail.com>
2026-04-02 21:57:26 +0200

    feat(authenticate): add logging and improve error management

--- app/authentication/handler.py
+++ app/authentication/handler.py
@@ -2,6 +2,10 @@ import json
 from shared.secrets_service import get_client_data, get_jwt_secret
 from shared.jwt_utils import generate_jwt
 from authentication.authenticate_client import validate_client
+from shared.logging_utils import log
+from shared.parse_utils import extract_method_and_path, get_correlation_id
+
+import os
 
 
 def handler(event: dict, context) -> dict:
@@ -22,42 +26,141 @@ def handler(event: dict, context) -> dict:
     Returns:
         dict: Dictionary with statusCode and body (JSON) containing the token or an error.
     """
-    # Parse the request body
-    body = json.loads(event.get("body", "{}"))
-    client_id = body.get("client_id")
-    client_secret = body.get("client_secret")
 
-    # Validate if body is empty:
-    if not client_id or not client_secret:
-        # Body vacío o campos faltantes
+    #Get secret name:
+    correlation_id = None
+
+    try:
+        correlation_id = get_correlation_id(event)
+        method, path = extract_method_and_path(event)
+
+        log(
+            "Authentication request received",
+            level="info",
+            correlation_id=correlation_id,
+            status="received",
+            method=method,
+            path=path
+        )
+
+        jwt_secret_name = os.getenv("JWT_SECRET_NAME", "auth/jwt_secret")
+        client_secret_prefix = os.getenv("CLIENT_SECRET_PREFIX","auth/client/")
+
+        # Parse the request body
+        body = json.loads(event.get("body", "{}"))
+        client_id = body.get("client_id")
+        client_secret = body.get("client_secret")
+
+        log(
+            "Authentication payload parsed",
+            level="info",
+            correlation_id=correlation_id,
+            method=method,
+            path=path,
+            client_id=client_id,
+            secret_present=bool(client_secret)
+        )
+
+
+        # Validate if body is empty:
+        if not client_id or not client_secret:
+            log(
+                "Missing client credentials",
+                level="warning",
+                correlation_id=correlation_id,
+                status="rejected",
+                method=method,
+                path=path,
+                client_id=client_id
+            )
+
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": "invalid_client"})
+            }
+        
+        # Retrieve client data from Secrets Manager
+        client_data = get_client_data(client_id)
+        if not client_data:
+            log(
+                "Client not found",
+                level="warning",
+                correlation_id=correlation_id,
+                status="rejected",
+                method=method,
+                path=path,
+                client_id=client_id
+            )
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": "invalid_client"})
+            }
+
+        # Validate client credentials
+        if not validate_client(client_id, client_secret, client_data):
+            log(
+                "Client authentication failed",
+                level="warning",
+                correlation_id=correlation_id,
+                status="rejected",
+                method=method,
+                path=path,
+                client_id=client_id
+            )
+
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": "invalid_client"})
+            }
+
+        log(
+            "Client authenticated successfully",
+            level="info",
+            correlation_id=correlation_id,
+            status="validated",
+            method=method,
+            path=path,
+            client_id=client_id
+        )
+
+        # Generate JWT token
+        token = generate_jwt(client_id, client_data.get("scope"), get_jwt_secret(SecretId=jwt_secret_name))
+
+        log(
+            "JWT generated successfully",
+            level="info",
+            correlation_id=correlation_id,
+            status="success",
+            method=method,
+            path=path,
+            client_id=client_id,
+            scopes=client_data.get("scope", [])
+        )
+
+        # Return success response with token
         return {
-            "statusCode": 401,
-            "body": json.dumps({"error": "invalid_client"})
+            "statusCode": 200,
+            "body": json.dumps({
+                "access_token": token,
+                "token_type": "Bearer"
+            })
+        }
+    except Exception as e:
+        import traceback
+
+        log(
+            "Authentication process failed",
+            level="error",
+            correlation_id=correlation_id,
+            status="failed",
+            method=method,
+            path=path,
+            error=str(e),
+            traceback=traceback.format_exc()
+        )
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "internal_server_error"})
         }
     
-    # Retrieve client data from Secrets Manager
-    client_data = get_client_data(client_id)
-    if not client_data:
-        return {
-            "statusCode": 401,
-            "body": json.dumps({"error": "invalid_client"})
-        }
-
-    # Validate client credentials
-    if not validate_client(client_id, client_secret, client_data):
-        return {
-            "statusCode": 401,
-            "body": json.dumps({"error": "invalid_client"})
-        }
-
-    # Generate JWT token
-    token = generate_jwt(client_id, client_data.get("scope"), get_jwt_secret())
-
-    # Return success response with token
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "access_token": token,
-            "token_type": "Bearer"
-        })
-    }
